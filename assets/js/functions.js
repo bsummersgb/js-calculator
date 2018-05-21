21782ded321bc561ac29d9e9b87758a215e4bd06
Bryan Summers <bsummersgb@gmail.com>
2018-05-22 07:21:20 +0800

    added logic for consecutive calculations - currently broken

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -17,7 +17,8 @@ const calculate = (n1, operator, n2) => {
         case 'divide':
             return parseFloat(n1) / parseFloat(n2);
             break;
-    } 
+    }
+    calculator.dataset.firstValue = n2;
 }
 
 
@@ -30,7 +31,7 @@ keys.addEventListener('click', event => {
     Array.from(key.parentNode.children)
         .forEach(key => key.classList.remove('is-depressed'));
 
-    if (event.target.matches('button')) {
+    if (key.matches('button')) {
         const previousKeyType = calculator.dataset.previousKeyType;
         if (!action) {
             if (displayedNumber === '0' || previousKeyType === 'operator') {
@@ -38,6 +39,7 @@ keys.addEventListener('click', event => {
             } else {
                 display.textContent += keyContent;
             }
+            calculator.dataset.previousKeyType = 'number';                        
         }
         if (
             action === 'add' ||
@@ -45,27 +47,52 @@ keys.addEventListener('click', event => {
             action === 'multiply' ||
             action === 'divide'
         ) { 
+            const firstValue = calculator.dataset.firstValue
+            const operator = calculator.dataset.operator
+            const secondValue = displayedNumber
+
+            if (firstValue && operator && previousKeyType !== 'operator') {
+                const calcValue = calculate(firstValue, operator, secondValue)
+                display.textContent = calcValue
+              
+                // Update calculated value as firstValue
+                calculator.dataset.firstValue = calcValue
+              } else {
+                // If there are no calculations, set displayedNum as the firstValue
+                calculator.dataset.firstValue = displayedNumber;
+              }
+
+
             key.classList.add('is-depressed');
-            calculator.dataset.previousKeyType = 'operator';
             calculator.dataset.firstValue = displayedNumber;
             calculator.dataset.operator = action;
+            calculator.dataset.previousKeyType = 'operator';            
         }
         if (action === 'decimal') {
-            if (display.textContent[display.textContent.length - 1] !== '.' && display.textContent.indexOf('.') == -1) {
+            if (display.textContent.indexOf('.') == -1) {
                 display.textContent += '.';
+            } 
+            if (previousKeyType === 'operator') {
+                display.textContent = '0.';
             } else {
                 return;
             }
+            calculator.dataset.previousKeyType = 'decimal';            
+            
         }
         if (action === 'calculate') { 
             const firstValue = calculator.dataset.firstValue;
             const operator = calculator.dataset.operator;
             const secondValue = displayedNumber;
+            calculator.dataset.previousKeyType = 'equals';         
 
             display.textContent = calculate(firstValue, operator, secondValue);
 
         }
-        if (action === 'clear') { console.log(`You hit the ${action} button mofo`) }
+        if (action === 'clear') { 
+            calculator.dataset.previousKeyType = 'clear';       
+            
+        }
     }
 });
 
